b359c36c29ffa062d745de014696b124480cdfb9
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-11-10 20:30:43 -0400

    added index page

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { data } from "../SpeakerData"
+import Speaker from "../src/components/Speaker";
+
+const IndexPage = () => {
+
+  return (
+
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map(speaker => {
+          return (
+            <Speaker key={speaker.id} speaker={speaker} />
+          )
+        })}
+      </div>
+    </div>
+  )
+};
+
+export default IndexPage;
