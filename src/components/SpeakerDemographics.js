de2e7f914c60f14dba5dd751e4fa061e9e89ce13
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-11-10 20:21:40 -0400

    added speacker component

---
+++ src/components/SpeakerDemographics.js
@@ -0,0 +1,28 @@
+
+
+const SpeakerDemographics = ({ first, last, company, bio, twitterHandle, favorite }) => {
+    return (
+        <div className="speaker-info">
+            <div className="d-flex justify-content-between mb-3">
+                <h3 className="text-truncate w-200">
+                    {first} {last}
+                </h3>
+            </div>
+            <p className="card-description">
+                {bio}
+            </p>
+            <div className="social d-flex flex-row mt-4">
+                <div className="company">
+                    <h5>Company</h5>
+                    <h6>{company}</h6>
+                </div>
+                <div className="twitter">
+                    <h5>Twitter</h5>
+                    <h6>{twitterHandle}</h6>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default SpeakerDemographics
