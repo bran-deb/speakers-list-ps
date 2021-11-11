a9b7e104c6b702af54d525292d982f2a4f098f31
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-11-10 21:28:45 -0400

    added toolbar component

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,10 +1,12 @@
 import { data } from "../../SpeakerData"
+import Header from "./Header"
 import SpeakersList from "./SpeakersList"
 
 
 const Speakers = () => {
     return (
         <div className="container-fluid">
+            <Header />
             <SpeakersList data={data} />
         </div>
     )
