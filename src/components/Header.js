871fe3842b168124958644d9c853d8409afed7bc
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-11-11 13:00:43 -0400

    modified ui text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,20 @@
-import React from 'react'
 
-const Header = () => {
+
+const Header = ({ theme }) => {
     return (
         <div className="padT4 padB4">
             <div className="container mobile-container">
                 <div className="d-flex justify-content-between">
                     <div>
-                        <img
-                            alt="SVCC Home Page"
-                            src="/imgaesSVCCLogo.png" />
+                        <img alt="SVCC Home Page" src="/images/SVCCLogo.png" />
                     </div>
                     <div className="light">
                         <h4 className="header-title">
                             Silicon Valley Code Camp
                         </h4>
                     </div>
-                    <div className="text-dark">
-                        Hello Mr. Smith &nbdp;&nbsp;
+                    <div className={theme === 'light' ? '' : 'text-info'}> {/*cambia color de letra */}
+                        Hello Mr. Smith &nbsp;&nbsp;
                         <span>
                             <a href="#">sign-out</a>
                         </span>
