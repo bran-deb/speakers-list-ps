eaba10d28fc8a00c583cb4c33fed0111d8379ca4
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-06 18:52:57 -0400

    error-boundary added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5671001e04444891"; }
+/******/ 		__webpack_require__.h = function() { return "3e332eb0819665c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
