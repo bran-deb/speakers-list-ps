dac95234d57d47cb34867e3b639dad91a151d4b8
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-06 17:42:29 -0400

    MEMO para evitar el re-rendering

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { memo, useContext } from "react"
 import { SpeakerFilterContext } from "../context/SpeakerFilterContext"
 import SpeakerDemographics from "./SpeakerDemographics"
 import SpeakerImg from "./SpeakerImg"
@@ -6,11 +6,17 @@ import Sessions from "./Sessions"
 import { SpeakerProvider } from "../context/SpeakerContext"
 import SpeakerDelete from "./SpeakerDelete"
 
+//TODO:
+//cuando se pasa una funcion por props el componente se vuelve
+//a renderizar por que no sabe si sus valores cambiaron
 
-const Speaker = ({ speaker, updateRecord, insertRecord, deleteRecord }) => {
+//memo renderiza unicamente cuando los props de Speaker cambian
+const Speaker = memo(({ speaker, updateRecord, insertRecord, deleteRecord }) => {
 
     //usamos react context para no pasar sessions en props
     const { showSessions } = useContext(SpeakerFilterContext)
+
+    console.log(`speaker: ${speaker.id} ${speaker.first} ${speaker.last}`);
 
     return (
         // pasamos el speaker y update record a todos los children por medio del provider
@@ -33,6 +39,11 @@ const Speaker = ({ speaker, updateRecord, insertRecord, deleteRecord }) => {
             </div>
         </SpeakerProvider>
     )
+}, areEqualSpeaker)
+
+//verifica que el favorite se haya actualizado o no
+function areEqualSpeaker(prevProps, nextProps) {
+    return (prevProps.speaker.favorite === nextProps.speaker.favorite)
 }
 
 export default Speaker
--- src/components/SpeakerImg.js
+++ src/components/SpeakerImg.js
@@ -14,10 +14,16 @@ const ImageWithFallback = ({ src, ...props }) => {
     return <img src={imgSrc} {...props} onError={onError} />
 }
 
+//FIXME:
+//al pasar el speaker por useContext crea una copia de speaker que
+//no sabe si los datos cambiaron
+//SpeakerImg se re-renderiza cada que hay un cambio en el Speaker
+//se puede usar memo para evitar pero la carga del render es pequeña
 const SpeakerImg = () => {
     //pasamos el speaker por context api
     const { speaker } = useContext(SpeakerContext);
     const { id, first, last } = speaker
+    console.log(`IMAGE: ${speaker.id} ${speaker.first} ${speaker.last}`);
 
     return (
         <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
