67a613958b438f1b83268719c25b5f155016f414
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-05 23:40:06 -0400

    Update useRequestDelay to persist updates with rest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -91,6 +91,18 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	!function() {
 /******/ 		// define getter functions for harmony exports
@@ -119,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "86c179d90a16abd5"; }
+/******/ 		__webpack_require__.h = function() { return "9cfab8acf0c72e0b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -132,6 +144,21 @@
 /******/ 				if (typeof window === 'object') return window;
 /******/ 			}
 /******/ 		})();
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/harmony module decorator */
+/******/ 	!function() {
+/******/ 		__webpack_require__.hmd = function(module) {
+/******/ 			module = Object.create(module);
+/******/ 			if (!module.children) module.children = [];
+/******/ 			Object.defineProperty(module, 'exports', {
+/******/ 				enumerable: true,
+/******/ 				set: function() {
+/******/ 					throw new Error('ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ' + module.id);
+/******/ 				}
+/******/ 			});
+/******/ 			return module;
+/******/ 		};
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
---
+++ src/hooks/useRequestRest.js
@@ -0,0 +1,131 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+export const REQUEST_STATUS = {
+    LOADING: "loading",
+    SUCCESS: "success",
+    FAILLURE: "failure",
+}
+
+const restUrl = 'api/speakers'
+
+function useRequestRest() {
+
+    const [data, setData] = useState(initialData)
+    const [requestStatus, setRequestStatus] = useState(REQUEST_STATUS.LOADING)
+    const [error, setError] = useState("")
+
+    const delay = (ms) => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms)
+        })
+    }
+
+    useEffect(() => {
+        async function delayfunc() {
+            try {
+                //obtiene los datos del RestUrl
+                const result = await axios.get(restUrl)
+                setRequestStatus(REQUEST_STATUS.SUCCESS)
+                setData(result.data)
+
+            } catch (e) {
+                setRequestStatus(REQUEST_STATUS.FAILLURE)
+                setHasErrored(true)
+                setError(e)
+            }
+        }
+        delayfunc()
+    }, [])
+
+    function updateRecord(record, doneCallback) {
+
+        const originalRecords = [...data]
+        const newRecords = data.map((rec) => {
+            return rec.id === record.id ? record : rec
+        })
+        async function delayFunction() {
+            try {
+                setData(newRecords)
+                // await delay(delayTime)
+                //agregamos al "urlRest/id" el registro(record) a actualizar
+                await axios.put(`${restUrl}/${record.id}`, record)
+                if (doneCallback) {
+                    doneCallback()
+                }
+
+            } catch (e) {
+                console.log("error throw inside delayFunction", error)
+                if (doneCallback) {
+                    doneCallback()
+                }
+                setData(originalRecords)
+            }
+        }
+        delayFunction()
+    }
+
+    function insertRecord(record, doneCallback) {
+
+        const originalRecords = [...data]
+        const newRecords = [record, ...data]
+        async function delayFunction() {
+            try {
+                setData(newRecords)
+                // await delay(delayTime)
+                //agregamos 99999 como marcador de posicion
+                //el server genera el speakerId, no tenemos id para pasarlo
+                await axios.post(`${restUrl}/99999`, record)
+                if (doneCallback) {
+                    doneCallback()
+                }
+
+            } catch (e) {
+                console.log("error throw inside delayFunction", error)
+                if (doneCallback) {
+                    doneCallback()
+                }
+                setData(originalRecords)
+            }
+        }
+        delayFunction()
+    }
+
+    function deleteRecord(record, doneCallback) {
+
+        const originalRecords = [...data]
+        const newRecords = data.filter((rec) => {
+            return rec.id != record.id
+        })
+        async function delayFunction() {
+            try {
+                setData(newRecords)
+                // await delay(delayTime)
+                //eliminamos del "urlRest/id" el (record)
+                await axios.delete(`${restUrl}/${record.id}`, record)
+                if (doneCallback) {
+                    doneCallback()
+                }
+
+            } catch (e) {
+                console.log("error throw inside delayFunction", error)
+                if (doneCallback) {
+                    doneCallback()
+                }
+                setData(originalRecords)
+            }
+        }
+        delayFunction()
+    }
+
+    return {
+        data,
+        requestStatus,
+        error,
+        updateRecord,
+        insertRecord,
+        deleteRecord,
+    }
+}
+export default useRequestRest
